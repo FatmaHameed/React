0f7d4e3958eac6fd020fb53b1363a2807abf0db3
Fatma Hameed <fatma78hameed@gmail.com>
2020-11-16 04:40:17 -0800

    commited changes in App.js

--- week2/react-exercises/src/App.js
+++ week2/react-exercises/src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 import Friend from './exercise1-NewFriend/Friend';
+import DogGallery from './exercise2-DogPhoto/dogGallery'
+import RandomJoke from './exercise3-RandomJoke/RandomJoke'
 
 function App() {
   return (
     <div className="App">
       
-      <Friend/>
+      <Friend />
+
+      <DogGallery/>
+
+      <RandomJoke/>
     </div>
     
   );
