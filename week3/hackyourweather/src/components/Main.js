b5eaab7a4ad5d995993f4e39fd3fbb794d4f0ed3
Fatma Hameed <fatma78hameed@gmail.com>
2020-11-21 23:09:44 -0800

    modified Main component

--- week3/hackyourweather/src/components/Main.js
+++ week3/hackyourweather/src/components/Main.js
@@ -7,12 +7,12 @@ import SearchDataButton from './button'
 
 
 const Main= () => {
-  const [city, setCity] = useState({});
+  const [city, setCity] = useState([]);
   const [cityName, setCityName] = useState('');
    const [isLoading, setLoading] = useState(false);
   const [isError, setError] = useState({});
+  let [key, setKey] = useState(0)
   const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
-  // console.log(API_KEY);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${API_KEY}&units=metric`
   const fetchData = async() => {
     try {
@@ -20,24 +20,22 @@ const Main= () => {
       const response = await fetch(url);
       const data = await response.json();
         if (data.cod < 400) {
-        setCity(data);
-        console.log(data)
+        setCity([...city, data]);
+        setKey(key++)
         setLoading(false);
+        setError({})
       } else {
-        setCity({});
         throw Error(data.cod);
       }
      await setCityName('');
     } catch (error) {
     console.log(error);
-    // console.log(err);
     setError(error.message);
     setLoading(true);
     setLoading(false);
-    }
+    } 
   }
 const error = Object.keys(isError).length = 0;
-const displayWeatherInfo = error || Object.keys(city).length > 0 ? <WeatherBox city = {city}/> : <ErrorMessage isError = {isError}/>
   return (
     <div>
       <span className="flex">
@@ -45,13 +43,17 @@ const displayWeatherInfo = error || Object.keys(city).length > 0 ? <WeatherBox c
       cityName={cityName}
       setCityName={setCityName}
       />
-      <SearchDataButton fetchData = {fetchData}/></span>
-    {/* <button onClick={FetchData}>Fetch City </button> */}
+      <SearchDataButton fetchData = {fetchData} cityName={cityName}/></span>
     {isLoading && <Spinner/>}
-    <div>{displayWeatherInfo}</div>
-    {/* {Object.keys(city).length > 0 && <WeatherBox city = {city}/>}
-    {error && <ErrorMessage />} */}
-<h1>{console.log("Here is the error", isError)}</h1>
+    {city.length > 0 &&  
+    <WeatherBox 
+                city = {city}
+                id= {key}
+                setCity={setCity}
+    />}
+    {!error && <ErrorMessage isError = {isError}/>}
+    <h1>{console.log("Here is the error", isError)}</h1>
+    <h1>{console.log("Here is the city array", city)}</h1>
     </div>)
 }
 export default Main; 
