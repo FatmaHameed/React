8877dfd842488045b3124109000a7362450e6a0a
Fatma Hameed <fatma78hameed@gmail.com>
2020-11-21 23:11:43 -0800

    modified App.js

--- week3/hackyourweather/src/App.js
+++ week3/hackyourweather/src/App.js
@@ -1,15 +1,12 @@
 import Title from './components/header'
 import './App.css';
 import Main from './components/Main';
-// import InputCity from './components/input'
 
 function App() {
   return (
     <div className="App">
       <header className="header">
       <Title title ="Weather" />
-      {/* <WeatherBox /> */}
-      {/* <InputCity /> */}
       <Main />
       </header>
     </div>
