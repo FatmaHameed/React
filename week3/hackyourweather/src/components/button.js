c25e9952f78811bd48a3b16d267274cfd8889cf1
Fatma Hameed <fatma78hameed@gmail.com>
2020-11-21 23:02:12 -0800

    modified button component, added disabled when there is no letters in the input fields

--- week3/hackyourweather/src/components/button.js
+++ week3/hackyourweather/src/components/button.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const SearchDataButton = ({fetchData}) => {
+const SearchDataButton = ({fetchData, cityName}) => {
   return <button 
   onClick={fetchData}
-  >Search</button>
+  disabled={cityName.trim() === "" ? true : false}
+  style={{ cursor: cityName.trim() === ""? "not-allowed": "pointer"}}
+>Search</button>
 };
 export default SearchDataButton;
