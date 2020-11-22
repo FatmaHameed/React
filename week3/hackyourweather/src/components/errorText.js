9317c23759607a38e7cecb4adbcee4e4c300976a
Fatma Hameed <fatma78hameed@gmail.com>
2020-11-21 23:08:05 -0800

    modified errorText on capital letters

--- week3/hackyourweather/src/components/errorText.js
+++ week3/hackyourweather/src/components/errorText.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const ErrorMessage = ({isError}) => {
   if (isError == 404) {
-    return <div className="error"><p>Please Input a Valid City Name</p></div>
+  return <div className="error"><p>PLEASE SEARCH A VALID CITY NAME</p></div>
   } else if (isError == 401) {
     return <div className="error"><p>Oops! Something Went Wrong</p></div>
-  } else {
+  } 
+  else {
   return <div>{null}</div>
   }
 }
